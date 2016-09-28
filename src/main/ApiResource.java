25f546eefb225ae696d271e2651affebf07a29f3
baxtex stat <baxtexx@gmail.com>
2016-09-28 11:03:04 +0200

    Managed to create a readme file and simple get request resource that works

--- src/main/ApiResource.java
+++ src/main/ApiResource.java
@@ -1,11 +1,25 @@
 package main;
 
-import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+@Path("/foo")
 public class ApiResource {
 
+	@GET
+	@Path("/bar")
+	@Produces(MediaType.TEXT_HTML)
+	public String getUsers() {
+		return "Testing";
+	}
+	
+	@GET
+	@Path("/bar2")
+	@Produces(MediaType.TEXT_HTML)
+	public String getUsers2() {
+		return "Testing";
+	}
 }
