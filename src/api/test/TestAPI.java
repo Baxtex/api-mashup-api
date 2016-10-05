6c2f118210296c1bd0513d79b705ac152ec32cb5
baxtex stat <baxtexx@gmail.com>
2016-10-05 18:18:42 +0200

    Added Twitter4J framwork

--- src/api/test/TestAPI.java
+++ src/api/test/TestAPI.java
@@ -1,5 +1,6 @@
 package api.test;
 
+import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -13,13 +14,17 @@ import org.glassfish.jersey.client.oauth2.ClientIdentifier;
 import org.glassfish.jersey.client.oauth2.OAuth2ClientSupport;
 import org.glassfish.jersey.client.oauth2.OAuth2CodeGrantFlow;
 import org.glassfish.jersey.client.oauth2.TokenResult;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 import org.glassfish.jersey.client.authentication.*;;
 
-
-
 /**
- * This class acts as a simple API.
- * The URI for accessing these resorces is:
+ * This class acts as a simple API. The URI for accessing these resorces is:
  * localhost:8080/api-mashup-api/api/v1/foo/
  * 
  * @author Anton Gustafsson
@@ -28,9 +33,23 @@ import org.glassfish.jersey.client.authentication.*;;
 
 @Path("/v1/foo")
 public class TestAPI {
+	
+	private TwitterFactory tf;
+	/**
+	 * Initializes Twitter framework.
+	 */
+	public TestAPI(){
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true).setOAuthConsumerKey("H3tHZ9FUpB2vmH9c61ZcfVyjg")
+				.setOAuthConsumerSecret("WvwnarPgqKJ2sMDKxH0tXLvR2N1gMpzNN484JHaYCeeW0lyJ8i")
+				.setOAuthAccessToken("4784213706-IqzIyd8oWhpYYrhaoNo18FmAg9BfjWRe79zk0gn")
+				.setOAuthAccessTokenSecret("SQLAqbZtd2MixfhoVXWGiqmBl1osGRSYV2SB8wHG0Bo0w");
+		tf = new TwitterFactory(cb.build());
+	}
 
 	/**
 	 * Prints to the screen if we are in /v1/foo
+	 * 
 	 * @return
 	 */
 	@GET
@@ -38,9 +57,10 @@ public class TestAPI {
 	public String print1() {
 		return "This should be printed if in foo";
 	}
-	
+
 	/**
 	 * Prints to the screen if we are in /v1/foo/bar
+	 * 
 	 * @return
 	 */
 	@GET
@@ -49,31 +69,32 @@ public class TestAPI {
 	public String print2() {
 		return "This should be printed if in foo/bar";
 	}
-	
+
 	/**
 	 * Prints to the screen if we are in /v1/foo/bar/1 else not.
+	 * 
 	 * @return
 	 */
 	@GET
 	@Path("/bar/{specific}")
 	@Produces(MediaType.TEXT_HTML)
 	public String print3(@PathParam("specific") int specific) {
-		if (specific == 1){
+		if (specific == 1) {
 			return "This should be printed if you looked for 1";
-		}else{
+		} else {
 			return "This should be printed if I found nothing, look for 1 instead!";
 		}
 	}
-	
+
 	/**
-	 * Creates a custom response and sends it as a JSON object
-	 * First we create a JSON object and we put boolean which is true into it.
-	 * If something goes wrong we build a new Response which says server error.
-	 * Else we build a response and returns our josnObject
-	 * (In this case, the error message is redundat as this code can never throw an error.
-	 * However, let's say we got data from a database, then that could
-	 * throw all kinds of errors, and we want to tell the user of the API
-	 * that something went wrong.
+	 * Creates a custom response and sends it as a JSON object First we create a
+	 * JSON object and we put boolean which is true into it. If something goes
+	 * wrong we build a new Response which says server error. Else we build a
+	 * response and returns our josnObject (In this case, the error message is
+	 * redundat as this code can never throw an error. However, let's say we got
+	 * data from a database, then that could throw all kinds of errors, and we
+	 * want to tell the user of the API that something went wrong.
+	 * 
 	 * @return
 	 * @throws Exception
 	 */
@@ -86,42 +107,63 @@ public class TestAPI {
 		try {
 			json.append("working", true);
 			returnString = json.toString();
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return Response.status(500).entity("Server was not able to process your request").build();
 		}
-		return Response.ok(returnString).build();	
+		return Response.ok(returnString).build();
 	}
-	
+
 	/**
-	 * Retrives bearer token and gets tweets if we are in /v1/foo/twitter
-	 * TODO: DOES NOT WORK! THROWS ERROR!
+	 * Retrives bearer token and gets tweets if we are in /v1/foo/twitter TODO:
+	 * DOES NOT WORK! THROWS ERROR!
+	 * 
 	 * @return
+	 * @throws TwitterException
 	 */
 	@GET
 	@Path("/twitter")
 	@Produces(MediaType.TEXT_HTML)
-	public String print5() {
-		// clientID, clientSecrect
-		ClientIdentifier ci = new ClientIdentifier("H3tHZ9FUpB2vmH9c61ZcfVyjg",
-				"WvwnarPgqKJ2sMDKxH0tXLvR2N1gMpzNN484JHaYCeeW0lyJ8i");
+	public String print5() throws TwitterException {
+		// // clientID, clientSecrect
+		// ClientIdentifier ci = new
+		// ClientIdentifier("H3tHZ9FUpB2vmH9c61ZcfVyjg",
+		// "WvwnarPgqKJ2sMDKxH0tXLvR2N1gMpzNN484JHaYCeeW0lyJ8i");
+		//
+		// // (clientId, authUri, accessTokenUri) på authcodegrantdflow
+		// System.out.println("Before flow");
+		// OAuth2CodeGrantFlow flow =
+		// OAuth2ClientSupport.authorizationCodeGrantFlowBuilder(ci,
+		// "https://api.twitter.com/oauth2/token",
+		// "https://api.twitter.com/oauth2/token")
+		// //.scope("contact")
+		// .build();
+		// System.out.println("After flow");
+		//
+		// String finalAuthorizationUri = flow.start();
+		// //TokenResult result = flow.finish(code, state);
+		//
+		//
+		// return "This is the finalAuthorizationUri: " +
+		// finalAuthorizationUri.toString();
 
-		// (clientId, authUri, accessTokenUri) på authcodegrantdflow
-		System.out.println("Before flow");
-		OAuth2CodeGrantFlow flow = OAuth2ClientSupport.authorizationCodeGrantFlowBuilder(ci,
-				"https://api.twitter.com/oauth2/token", "https://api.twitter.com/oauth2/token")
-				//.scope("contact")
-				.build();
-		System.out.println("After flow");
+//		ConfigurationBuilder cb = new ConfigurationBuilder();
+//		cb.setDebugEnabled(true).setOAuthConsumerKey("H3tHZ9FUpB2vmH9c61ZcfVyjg")
+//				.setOAuthConsumerSecret("WvwnarPgqKJ2sMDKxH0tXLvR2N1gMpzNN484JHaYCeeW0lyJ8i")
+//				.setOAuthAccessToken("4784213706-IqzIyd8oWhpYYrhaoNo18FmAg9BfjWRe79zk0gn")
+//				.setOAuthAccessTokenSecret("SQLAqbZtd2MixfhoVXWGiqmBl1osGRSYV2SB8wHG0Bo0w");
+//		TwitterFactory tf = new TwitterFactory(cb.build());
+		Twitter twitter = tf.getInstance();
 
-		String finalAuthorizationUri = flow.start();
-		//TokenResult result = flow.finish(code, state);
-		
+		String result = null;
+		List<Status> statuses = twitter.getUserTimeline("@annieloof");
+		for (Status status : statuses) {
 
-		return "This is the bearer: " + finalAuthorizationUri.toString();
+			result += status.getUser().getName() + ":" + status.getText();
+		}
+
+		return result;
+
 	}
-	
-	
-	
+
 }
