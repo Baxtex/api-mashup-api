71e70fcca0772454fc07757e4bb123621f8f5dae
Kajsa Ornstein <Kajsa Ornstein>
2016-10-13 13:28:43 +0200

    added empty method in controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,8 +1,36 @@
 package controller;
 
-import database.*;
-import externalAPIs.*;
+import javax.ws.rs.core.Response;
 
 public class Controller {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// Metoder som Kajsa behöver
+	
+	/**
+	 * Returns a JSON response containing images of all the party logos
+	 * @return
+	 */
+	
+	public Response getPartyLogos() {
+		return null;
+	}
 	
 }
